2a6f58ef89a8c9f921c3f89a0ea54bb8f90f15f5
DiscordAnaxes <anaxesoce@gmail.com>
2021-08-04 20:57:06 +1000

    Attempting something.

---
+++ api/premid.php
@@ -0,0 +1,18 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+// make request
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, 'https://api.premid.app/v2/presenceUsage');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+echo($output);
+
+// handle error; error output
+if(curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200) {
+
+    var_dump($output);
+}
+
+curl_close($ch);
+?>
