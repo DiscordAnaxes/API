7f059b53d724dbd10ecc920cdebd538f5094d383
DiscordAnaxes <anaxesoce@gmail.com>
2021-08-06 10:34:44 +1000

    Updated.

---
+++ assets/js/premid.js
@@ -0,0 +1,12 @@
+fetch("https://api.anaxes.xyz/api/premid.php").then(function (response) {
+  if (response.ok) {
+    // Check if response went through
+    response.json().then(function (data) {
+      var twitterFollowersHTML = document.getElementById("dsc_users");
+      var followers = `${`${data.WidgetStyle +  " Users"}` || "💻 404"}`;
+      twitterFollowersHTML.innerHTML += followers;
+    });
+  } else {
+    console.log("Response failed?");
+  }
+});
