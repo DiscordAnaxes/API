fd57e5b1f377bb5c187cb9effbf8913b62469f41
Anaxes <68460474+DiscordAnaxes@users.noreply.github.com>
2021-08-28 16:20:29 +1000

    Update.

--- assets/js/lanyard.js
+++ assets/js/lanyard.js
@@ -5,12 +5,12 @@ fetch("https://api.anaxes.xyz/api/lanyard.php").then(function (response) {
         var discordStatusHTML = document.getElementById("lanyard_status");
         var status = `${
           data.data.discord_status === "online"
-            ? "<i class='fad fa-circle text-green-500 animate-pulse' title='Online'></i>&nbsp;Online"
+            ? "<span class='text-green-500'><i class='fad fa-circle animate-pulse' title='Online'></i>&nbsp;Online</span>"
             : data.data.discord_status === "dnd"
-            ? "<i class='fad fa-circle text-red-500 animate-pulse' title='Do Not Disturb'></i>&nbsp;Offline"
+            ? "<span class='text-red-500'><i class='fad fa-circle animate-pulse' title='Do Not Disturb'></i>&nbsp;Offline</span>"
             : data.data.discord_status === "idle"
-            ? "<i class='fad fa-circle text-yellow-500 animate-pulse' title='Idle'></i>&nbsp;Idle"
-            : "<i class='fad fa-circle text-gray-500 animate-pulse' title='Offline'></i>&nbsp;Offline"
+            ? "<span class='text-yellow-500'><i class='fad fa-circle animate-pulse' title='Idle'></i>&nbsp;Idle</span>"
+            : "<span class='text-gray-500'><i class='fad fa-circle animate-pulse' title='Offline'></i>&nbsp;Offline</span>"
         }`;
         discordStatusHTML.innerHTML += status;
       });
