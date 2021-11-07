9510b4247b4343f17a40d82595581bae5c0c91a8
Anaxes <68460474+DiscordAnaxes@users.noreply.github.com>
2021-11-07 16:21:24 +1000

    Update url.

--- assets/js/lanyard.js
+++ assets/js/lanyard.js
@@ -1,4 +1,4 @@
-fetch("https://api.anaxes.xyz/api/lanyard.php").then(function (response) {
+fetch("/api/lanyard.php").then(function (response) {
     if (response.ok) {
       // Check if response went through
       response.json().then(function (data) {
